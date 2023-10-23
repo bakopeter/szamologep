dd596fa465a80e0f1757e41e07579176060dfe7d
bakopeter <105720278+bakopeter@users.noreply.github.com>
2023-10-23 12:25:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+let szam1 = "0";
+let szam2 = "0";
+let eredmeny = 0;
+let helyes = false;
+let operator = "";
+
+let operatorSpan = document.querySelector("#operator-span");
+operatorSpan.addEventListener("click", function (event) {
+    operator = event.target.value;
+})
+
+function Beker() {
+
+    let regex = /^([-]?([0]|([0-9][,][0-9]+)|([1-9]+([,][0-9])?[0-9]*)))$/;
+
+    szam1 = document.getElementById("szam01").value;
+    szam2 = document.getElementById("szam02").value;
+
+    if (!szam1.match(regex) || !szam2.match(regex)) {
+        document.getElementById("szam01").value = "";
+        document.getElementById("szam02").value = "";
+        window.alert("Nem adtál meg megfelelő számot. Próbáld újra!");
+    }
+}
+
+function Szamol() {
+    Beker();
+
+    szam1 = Number(szam1);
+    szam2 = Number(szam2);
+
+    switch (operator) {
+        case "+":
+            eredmeny = szam1 + szam2;
+            break;
+        case "-":
+            eredmeny = szam1 - szam2;
+            break;
+        case "*":
+            eredmeny = szam1 * szam2;
+            break;
+        case "/":
+            if (szam2 == 0) {
+                window.alert("Zéró osztó!");
+            } else {
+                eredmeny = szam1 / szam2;
+            }
+            break;
+        case "^":
+            eredmeny = Math.pow(szam1, szam2);
+            break;
+        case "√":
+            eredmeny = Math.pow(szam2, 1/szam1);
+            break;
+        default: 0;
+    }
+
+    Kiir();
+}
+
+function Kiir() {
+    document.getElementById("ki").innerHTML = eredmeny;
+}
+
